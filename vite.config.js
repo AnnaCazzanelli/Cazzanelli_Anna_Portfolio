373331ff19ea5d6b78cd5e63d9cab0f09fdb0eb4
Anna <anna.cazzanelli1@gmail.com>
2026-06-15 11:53:21 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// COMMENTATO: Disattivo l'import del plugin per i test di accessibilità
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // COMMENTATO: Disattivo l'esecuzione del plugin così non inietta codice estraneo
+    // vueDevTools(),
   ],
   resolve: {
     alias: {
